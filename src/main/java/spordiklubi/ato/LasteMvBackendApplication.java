e663d5010b0aa1b36fd50d5b14ddc1ad66f0c92b
Olev Abel <olev.abel@lab.mobi>
2016-07-18 14:59:12 +0300

    initial commit

---
+++ src/main/java/spordiklubi/ato/LasteMvBackendApplication.java
@@ -0,0 +1,12 @@
+package spordiklubi.ato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasteMvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasteMvBackendApplication.class, args);
+	}
+}
