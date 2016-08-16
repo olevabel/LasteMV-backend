8624b41545a56600923c7af9b785a8eb5bb1faff
Olev Abel <olev.abel@lab.mobi>
2016-08-16 19:36:46 +0300

    test headers

--- src/main/java/spordiklubi/ato/network/controller/CompetitorController.java
+++ src/main/java/spordiklubi/ato/network/controller/CompetitorController.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import spordiklubi.ato.dto.Competitor;
 import spordiklubi.ato.dto.repositories.CompetitorRepository;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by olevabel on 7/19/16.
@@ -36,9 +38,25 @@ public class CompetitorController {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
         competitorRepository.save(competitor);
-        return new ResponseEntity<>(competitor, HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        return new ResponseEntity<>(competitor, headers, HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/competitors")
-    public ArrayList<Competitor> getCompetitors() { return this.competitorRepository.findAll();}
+    public ResponseEntity<ArrayList<Competitor>> getCompetitors() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        return new ResponseEntity<>(this.competitorRepository.findAll(), headers, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/competitors")
+    public ResponseEntity<Competitor> editCompetitor(@RequestBody Competitor competitor) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        competitorRepository.save(competitor);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        return new ResponseEntity<>(competitor, headers, HttpStatus.OK);
+    }
 }
